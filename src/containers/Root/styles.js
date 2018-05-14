7e31d61ef7a9fe87184142e922f83d2232ec473e
Chuma Azinge <chumaazinge@Chumas-MacBook-Pro.local>
2018-05-13 17:26:46 -0700

    Update Sample Text

--- src/containers/Root/styles.js
+++ src/containers/Root/styles.js
@@ -1,5 +1,19 @@
 import { StyleSheet } from 'react-native';
+import { Constants } from 'expo';
 
 export default StyleSheet.create({
-  main: { justifyContent: 'center' },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
 });
